c960a844e5e6556530ce26b49f8f62972c205adf
KireMitrov <kire_val@yahoo.com>
2024-01-02 13:09:43 +0100

    Navbar initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './navbar.css';
+import { navBarData } from './navBarData';
+
+export const Navbar = () => {
+    console.log(navBarData);
+
+
+    return (
+        <header className='header-main'>
+            <div className='header-container'>
+                <div className="logo">
+                    <a href="/">
+                        <img src="https://default.houzez.co/wp-content/uploads/2016/03/logo-houzez-white.png" alt="logo" />
+                    </a>
+                </div>
+                <div className="main-nav">
+                    <ul>
+                        {navBarData.map((item) => (
+                            <li key={item.name}>{item.title}
+                            <span className='arrow down'></span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="login-section">
+                    <ul>
+                        <li>icon</li>
+                        <li>
+                            <button href='/' className='create-listing-btn'>CREATE A LISTING</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
