9597e25c4b4da4b8bad38368d529e741dabeba10
KireMitrov <kire_val@yahoo.com>
2024-01-29 23:27:27 +0100

    Trying to fix deployment

--- src/Pages/About/components/blog/Blog.js
+++ src/Pages/About/components/blog/Blog.js
@@ -1,76 +1,76 @@
-import React from 'react'
-import "./blog.css"
-import blog1 from "../../../../assets/images/homePage/slider/home2.jpg"
+// import React from 'react'
+// import "./blog.css"
+// import blog1 from "../../../../assets/images/homePage/slider/home2.jpg"
 
-function Blog() {
-  return (
-    <div className='blog-main-container'>
-        <div className='wrapper'>
-          <div className="about-header">
-              <h2>Read From Our Blog</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
-          </div>
-          <div className="blog-cards-main-container">
-            <div className="blog-card">
-              <img src={blog1} alt="" />
-              <div className="blog-card-body">
-                <div className="blog-title">
-                10 Quick Tips About Business Development
-                </div>
-                <div className="blog-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
-                </div>
-                <div className="blog-link">
-                  Continue reading
-                </div>
-              </div>
-            </div>
-            <div className="blog-card">
-              <img src={blog1} alt="" />
-              <div className="blog-card-body">
-                <div className="blog-title">
-                10 Quick Tips About Business Development
-                </div>
-                <div className="blog-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
-                </div>
-                <div className="blog-link">
-                  Continue reading
-                </div>
-              </div>
-            </div>
-            <div className="blog-card">
-              <img src={blog1} alt="" />
-              <div className="blog-card-body">
-                <div className="blog-title">
-                10 Quick Tips About Business Development
-                </div>
-                <div className="blog-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
-                </div>
-                <div className="blog-link">
-                  Continue reading
-                </div>
-              </div>
-            </div>
-            <div className="blog-card">
-              <img src={blog1} alt="" />
-              <div className="blog-card-body">
-                <div className="blog-title">
-                10 Quick Tips About Business Development
-                </div>
-                <div className="blog-text">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
-                </div>
-                <div className="blog-link">
-                  Continue reading
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-  )
-}
+// function Blog() {
+//   return (
+//     <div className='blog-main-container'>
+//         <div className='wrapper'>
+//           <div className="about-header">
+//               <h2>Read From Our Blog</h2>
+//               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+//           </div>
+//           <div className="blog-cards-main-container">
+//             <div className="blog-card">
+//               <img src={blog1} alt="" />
+//               <div className="blog-card-body">
+//                 <div className="blog-title">
+//                 10 Quick Tips About Business Development
+//                 </div>
+//                 <div className="blog-text">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
+//                 </div>
+//                 <div className="blog-link">
+//                   Continue reading
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="blog-card">
+//               <img src={blog1} alt="" />
+//               <div className="blog-card-body">
+//                 <div className="blog-title">
+//                 10 Quick Tips About Business Development
+//                 </div>
+//                 <div className="blog-text">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
+//                 </div>
+//                 <div className="blog-link">
+//                   Continue reading
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="blog-card">
+//               <img src={blog1} alt="" />
+//               <div className="blog-card-body">
+//                 <div className="blog-title">
+//                 10 Quick Tips About Business Development
+//                 </div>
+//                 <div className="blog-text">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
+//                 </div>
+//                 <div className="blog-link">
+//                   Continue reading
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="blog-card">
+//               <img src={blog1} alt="" />
+//               <div className="blog-card-body">
+//                 <div className="blog-title">
+//                 10 Quick Tips About Business Development
+//                 </div>
+//                 <div className="blog-text">
+//                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis et sem sed sollicitudin. Donec...
+//                 </div>
+//                 <div className="blog-link">
+//                   Continue reading
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//     </div>
+//   )
+// }
 
-export default Blog
+// export default Blog
