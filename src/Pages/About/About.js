352f4eeb7c71c32d5942592f5752cc933a224093
KireMitrov <kire_val@yahoo.com>
2024-02-02 12:41:50 +0100

    Finished CSS for the about page team component

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Fade } from 'react-awesome-reveal'
 import "./about.css"
 import Testimonials from "../Home/components/testimonials/Testimonials"
 import Footer from '../../components/Footer/Footer'
@@ -10,25 +11,29 @@ function About() {
 
     return (
         <div>
-            <section className='about-top-container'>
-                <div className="contact-background-overlay"></div>
-                <h1>About us</h1>
-            </section>
-            <section>
-                <div className='wrapper about-text'>
-                    <div className="about-text-header">Your Vision Unrestricted</div>
-                    <div className="about-text-body">
-                        <div>
-                            <div><strong>Houzez is a premium WordPress theme for real estate agents and agencies</strong> where modern aesthetics are combined with a tasteful simplicity and where the ease of use is achieved without compromise in your ability to customize the design.</div>
-                            <div>Whether you are a real estate agent looking to build a website for your company or a web developer seeking a perfect WordPress theme for your next project, you are certain to appreciate the numerous features and benefits that our theme provides.</div>
-                        </div>
-                        <div>
-                            <div>Houzez is also a WordPress-based property management system which allows you to own and maintain a real estate marketplace, coordinate your agents, accept submissions and offer membership packages.</div>
-                            <div>Unlike many other real estate themes which confine you to a handful of predefined layouts, <strong>Houzez offers a limitless array of possibilities to structure and style your content.</strong> All of the customization options are logically organized in your WordPress admin panel and thorough customization in the provided documentation.</div>
+            <Fade duration={2500}>
+                <section className='about-top-container'>
+                    <div className="contact-background-overlay"></div>
+                    <h1>About us</h1>
+                </section>
+            </Fade>
+            <Fade duration={2500}>
+                <section>
+                    <div className='wrapper about-text'>
+                        <div className="about-text-header">Your Vision Unrestricted</div>
+                        <div className="about-text-body">
+                            <div className='row'>
+                                <div><strong>Houzez is a premium WordPress theme for real estate agents and agencies</strong> where modern aesthetics are combined with a tasteful simplicity and where the ease of use is achieved without compromise in your ability to customize the design.</div>
+                                <div>Whether you are a real estate agent looking to build a website for your company or a web developer seeking a perfect WordPress theme for your next project, you are certain to appreciate the numerous features and benefits that our theme provides.</div>
+                            </div>
+                            <div className='row'>
+                                <div>Houzez is also a WordPress-based property management system which allows you to own and maintain a real estate marketplace, coordinate your agents, accept submissions and offer membership packages.</div>
+                                <div>Unlike many other real estate themes which confine you to a handful of predefined layouts, <strong>Houzez offers a limitless array of possibilities to structure and style your content.</strong> All of the customization options are logically organized in your WordPress admin panel and thorough customization in the provided documentation.</div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </Fade>
             <Team />
             <Services />
             <Blog />
--- src/Pages/About/components/team/Team.js
+++ src/Pages/About/components/team/Team.js
@@ -7,6 +7,29 @@ import team4 from "../../../../assets/images/aboutPage/team4.jpg"
 
 function Team() {
 
+  const agents = [
+    {
+      img: team1,
+      name: "Kathleen Grant",
+      ocupation: "Funder",
+    },
+    {
+      img: team2,
+      name: "Keith Bailey",
+      ocupation: "CEO",
+    },
+    {
+      img: team3,
+      name: "Danielle Murray",
+      ocupation: "Manager",
+    },
+    {
+      img: team4,
+      name: "Thomas Stevens",
+      ocupation: "Manager",
+    },
+  ]
+
   return (
     <div className='teams-main-container'>
       <div className='wrapper'>
@@ -15,42 +38,33 @@ function Team() {
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
         </div>
         <div className='team-card-container'>
-          <div className='team-card'>
-            <img src={team1} alt="team member" />
-            <div class="team-content">
-              <div>
-                <strong>Kathleen Grant</strong>
+          {agents.map((agent) => (
+            <div className='team-card'>
+              <div className='team-card-overlay'>
+                <img src={agent.img} alt="team member" />
+                <div class="member-name">
+                  <div>
+                    <strong>{agent.name}</strong>
+                  </div>
+                  <div>{agent.ocupation}</div>
+                </div>
+                <div className='member-description'>
+                  <div>
+                    <div><strong>{agent.name}</strong></div>
+                    <div>{agent.ocupation}</div>
+                  </div>
+                  <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In malesuada, odio sit amet pharetra vehicula, sapien leo egestas magna, vitae auctor diam magna cursus arcu. </div>
+                  <div className='team-social-media'>
+                    <i className='fab fa-facebook-f'></i>
+                    <i className='fab fa-twitter'></i>
+                    <i className='fab fa-linkedin-in'></i>
+                    <i className='fab fa-pinterest-p'></i>
+                    <i className='fab fa-google-plus-g'></i>
+                  </div>
+                </div>
               </div>
-              <div>Funder</div>
             </div>
-          </div>
-          <div className='team-card'>
-            <img src={team2} alt="team member" />
-            <div class="team-content">
-              <div>
-                <strong>Keith Bailey</strong>
-              </div>
-              <div>CEO</div>
-            </div>
-          </div>
-          <div className='team-card'>
-            <img src={team3} alt="team member" />
-            <div class="team-content">
-              <div>
-                <strong>Danielle Murray</strong>
-              </div>
-              <div>Manager</div>
-            </div>
-          </div>
-          <div className='team-card'>
-            <img src={team4} alt="team member" />
-            <div class="team-content">
-              <div>
-                <strong>Thomas Stevens</strong>
-              </div>
-              <div>Manager</div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
