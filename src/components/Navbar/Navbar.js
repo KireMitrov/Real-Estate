da1a75c76b70fa918aaab648e24e4c47e700df72
KireMitrov <kire_val@yahoo.com>
2024-02-14 23:07:01 +0100

    Responsive nav bar initial commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,8 +6,7 @@ import Menuitems from './components/menuItems/Menuitems';
 
 export const Navbar = () => {
 
-    const [ showFavorites, SetShowFavorites] = useState(false);
-    console.log(showFavorites)
+    const [showFavorites, SetShowFavorites] = useState(false);
     const depthLevel = 0;
 
     return (
@@ -30,7 +29,7 @@ export const Navbar = () => {
                     <ul className='login-section-ul'>
                         <li className='login-icon' id='login-icon' onMouseEnter={() => SetShowFavorites(true)} onMouseLeave={() => SetShowFavorites(false)} >
                             <i class="fa-regular fa-circle-user" ></i>
-                            <div className={`favorites ${ showFavorites ? "show-favorites" : ""}`}>
+                            <div className={`favorites ${showFavorites ? "show-favorites" : ""}`}>
                                 <i className='fa-regular fa-heart'></i>
                                 FAVORITES
                                 <span className='favorites-number'> 0 </span>
@@ -40,6 +39,24 @@ export const Navbar = () => {
                             <button href='/' className='create-listing-btn'>CREATE A LISTING</button>
                         </li>
                     </ul>
+                </div>
+            </div>
+            <div className="resp-header-container">
+                <ul className='resp-nav-ul'>
+                    {navBarData.map((item, index) => (
+                        <Menuitems items={item} key={index} depthLevel={depthLevel}></Menuitems>
+                    ))}
+                </ul>
+                <div className='resp-nav-right'>
+                    <button className="resp-btn"><i className="fa fa-bars"></i></button>
+                    <div className="resp-logo">
+                        <a href="/">
+                            <img src="https://default.houzez.co/wp-content/uploads/2016/03/logo-houzez-color.png" alt="logo" />
+                        </a>
+                    </div>
+                    <div>
+                        <i class="fa-regular fa-circle-user"></i>
+                    </div>
                 </div>
             </div>
         </header>
